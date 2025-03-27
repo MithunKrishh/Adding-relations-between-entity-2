326e5a64d63e553709d70563a11e1b3926fc8460
MithunKrishh <mithunkannan06@gmail.com>
2025-03-27 10:40:18 +0530

    commited

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import RatingWidget from "./RatingWidget";
+import PropTypes from "prop-types";
+
+const ProductCard = ({ product, onRatingSubmit }) => {
+  return (
+    <div className="product-card">
+      <img src={product.image} alt={product.name} />
+      <h2>{product.name}</h2>
+      <p>{product.description}</p>
+      <p>Average Rating: ⭐ {product.avgRating} ({product.totalRatings} ratings)</p>
+      <RatingWidget productId={product.id} onRatingSubmit={onRatingSubmit} />
+    </div>
+  );
+};
+
+ProductCard.propTypes = {
+  product: PropTypes.object.isRequired,
+  onRatingSubmit: PropTypes.func.isRequired,
+};
+
+export default ProductCard;
